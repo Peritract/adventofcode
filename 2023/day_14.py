fd64cd2d650622a8c29d357b482dd05a9965c122
Dan <peritract@hotmail.co.uk>
2023-12-16 13:33:22 +0000

    Fix day

--- 2023/day_14.py
+++ 2023/day_14.py
@@ -129,7 +129,7 @@ def second_star(data):
 
 def solution(source):
     data = load_input(source)
-    print("Day N")
+    print("Day 14")
     print("First star:", str(first_star(data)))
     print("Second star:", str(second_star(data)))
     print("-------------------------------------")
