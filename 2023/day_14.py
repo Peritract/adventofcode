2fb67c4053a8eee2e5dd34a75806c86447d5239a
Dan <peritract@hotmail.co.uk>
2023-12-16 13:33:07 +0000

    Intuition, no understanding

--- 2023/day_14.py
+++ 2023/day_14.py
@@ -1,8 +1,9 @@
 from functools import cmp_to_key
+from collections import defaultdict
 
 def load_input(source):
     with open(source) as f:
-        data = [x.strip() for x in f.readlines()]
+        data = [list(x.strip()) for x in f.readlines()]
     if len(data) == 1:
         data = data[0]
     return data
@@ -12,32 +13,32 @@ def display_grid(grid):
         print("".join(y))
     print("")
 
-def sort_rocks(b, a):
-    if b == "O" and a == ".":
-        return -1
-    elif b == "." and a == "O":
-        return 1
-    return 0
 
-def roll(grid, dir):
-    if dir == "W":
-        return [sorted(row,
-                key=cmp_to_key(sort_rocks))
-                for row in grid]
-    elif dir == "E":
-        return [sorted(row[::-1],
-                key=cmp_to_key(sort_rocks))[::-1]
-                for row in grid]
+def tilt_west(grid):
+    for y in range(len(grid)):
+        row = grid[y]
+        for x in range(len(grid[0])):
+            if row[x] == "O":
+                o = x - 1
+                while row[o] == "." and o >= 0:
+                    o -= 1
+                row[o + 1], row[x] = row[x], row[o + 1]
+    return grid
 
 
-def rotate(grid):
-    n_grid = []
-    for x in range(len(grid[0])):
-        n_grid.append([grid[y][x] for y in range(len(grid))])
-    return n_grid
+def tilt_east(grid):
+    for y in range(len(grid)):
+        row = grid[y]
+        for x in range(len(grid[0]) - 1, -1, -1):
+            if row[x] == "O":
+                o = x + 1
+                while o < len(grid[0]) and row[o] == ".":
+                    o += 1
+                row[o - 1], row[x] = row[x], row[o - 1]
+    return grid
 
-   
-def tilt(grid):
+
+def tilt_north(grid):
     cols = []
     for c in range(len(grid[0])):
         col = [y[c] for y in grid]
@@ -53,11 +54,44 @@ def tilt(grid):
         row = ""
         for x in range(len(grid[0])):
             row += cols[x][y]
-        n_grid.append(row)
+        n_grid.append(list(row))
     return n_grid
     
+
+def tilt_south(grid):
+    cols = []
+    for c in range(len(grid[0])):
+        col = [y[c] for y in grid]
+        for x in range(len(col) -1, -1, -1):
+            if col[x] == "O":
+                o = x + 1
+                while o < len(col) and col[o] == ".":
+                    o += 1
+                col[o - 1], col[x] = col[x], col[o - 1]
+        cols.append(col)
+    n_grid = []
+    for y in range(len(grid)):
+        row = ""
+        for x in range(len(grid[0])):
+            row += cols[x][y]
+        n_grid.append(list(row))
+    return n_grid
+
+
+def spin(grid):
+    for func in [tilt_north, tilt_west, tilt_south, tilt_east]:
+        grid = func(grid)
+    return grid
+
+
+def string_rep(grid):
+    return "|".join("-".join(y) for y in grid)
+
+def reconstruct(sr):
+    return [r.split("-") for r in sr.split("|")]
+
 def first_star(data):
-    grid = tilt(data)
+    grid = tilt_north(data)
     tot = sum([r.count("O") * (len(grid) - i)
            for i, r in enumerate(grid)])
 
@@ -65,9 +99,33 @@ def first_star(data):
 
 
 def second_star(data):
-    display_grid(roll(data, "W"))
-    display_grid(roll(data, "E"))
+    grid = data
+    grids = []
+    dupes = defaultdict(lambda: [])
+    for i in range(1000):
+        grid = spin(grid)
+        sr = string_rep(grid)
+        if sr not in grids:
+            grids.append(sr)
+        else:
+            dupes[grids.index(sr)].append(i)
+    start = list(dupes.keys())[0]
+    step = [v[1] - v[0] for v in dupes.values()][0]
+    to_run = 1000000000 - start - 1
+    grid = data
+    for i in range(start):
+        grid = spin(grid)
+    patterns = []
+    for c in range(0, len(dupes.keys())):
+        grid = spin(grid)
+        patterns.append(string_rep(grid))
+        tot = sum([r.count("O") * (len(grid) - i)
+           for i, r in enumerate(grid)])
+    result = reconstruct(patterns[to_run % len(patterns)])
+    tot = sum([r.count("O") * (len(result) - i)
+        for i, r in enumerate(result)])
 
+    return tot
 
 def solution(source):
     data = load_input(source)
