cf7da07f6de1c631e5f8f03adab3aff4d1f6b370
Dan Keefe <peritract@hotmail.co.uk>
2024-03-21 14:25:06 +0000

    Update day_2.py

--- hannukah_of_data/day_2.py
+++ hannukah_of_data/day_2.py
@@ -5,7 +5,7 @@ import pandas as pd
 if __name__ == "__main__":
 
     query = """
-        SELECT C.name, C.phone, C.citystatezip, COUNT(*)
+        SELECT C.name, C.phone
         FROM orders AS O
         LEFT JOIN customers AS C
         ON (O.customerid = C.customerid)
@@ -13,15 +13,15 @@ if __name__ == "__main__":
         ON (OI.orderid = O.orderid)
         LEFT JOIN products AS P
         ON (OI.sku = P.sku)
-        WHERE C.name LIKE 'J% D%'
-        AND (P.desc LIKE '%bagel%' OR P.desc LIKE 'Coffee%')
-        GROUP BY C.customerid
-        ORDER BY COUNT(*) DESC
-        LIMIT 1;
+        WHERE C.name LIKE 'J% P%'
+        AND (P.desc LIKE '%bagel%' OR P.desc LIKE '%coffee%')
+        AND STRFTIME('%Y', O.shipped) = '2017'
+        GROUP BY C.customerid, O.orderid
+        HAVING COUNT(*) >= 2
     """
 
     con = connect("./data/noahs.sqlite")
 
     df = pd.read_sql(query, con)
 
-    print(df.iloc[0][["phone", "citystatezip"]])
+    print(df)
